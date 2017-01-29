3a3442d558a438379e6b16285b3070334b242d95
Sandra Pallarés <sandra.pallares.gual@gmail.com>
2017-01-29 19:12:19 +0100

    US#5 - refresh todo list

--- FluToDo/FluToDo/ViewModels/TodoListViewModel.cs
+++ FluToDo/FluToDo/ViewModels/TodoListViewModel.cs
@@ -9,6 +9,7 @@ namespace FluToDo.ViewModels
     internal class TodoListViewModel : ViewModelBase
     {
         private ITodoManager todoManager;
+        private bool _isRefreshing = false;
 
         public TodoListViewModel(ITodoManager todoManager)
         {
@@ -20,6 +21,22 @@ namespace FluToDo.ViewModels
             this.NewTodoCommand = new Command(async () => { await App.NavigationService.PushAsync(new TodoCreationPage()); });
             this.DeleteCommand = new Command<TodoViewModel>(async (todo) => { await this.todoManager.DeleteTodoAsync(todo); });
             this.DoneCommand = new Command<TodoViewModel>(async (todo) => { await this.todoManager.UpdateTodoAsync(todo); });
+            this.RefreshCommand = new Command(async () =>
+            {
+                IsRefreshing = true;
+                await this.todoManager.LoadTodosAsync();
+                IsRefreshing = false;
+            });
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this._isRefreshing; }
+            set
+            {
+                this._isRefreshing = value;
+                this.RaisePropertyChanged();
+            }
         }
 
         public ObservableCollection<TodoViewModel> TodoList { get; private set; }
@@ -28,5 +45,6 @@ namespace FluToDo.ViewModels
         public ICommand NewTodoCommand { get; private set; }
         public ICommand DeleteCommand { get; private set; }
         public ICommand DoneCommand { get; private set; }
+        public ICommand RefreshCommand { get; private set; }
     }
 }
